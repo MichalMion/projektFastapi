2cb5fd923f41a62f4cf1b2628901576b29f59bf4
MichalMion <144840332+MichalMion@users.noreply.github.com>
2025-11-30 14:57:02 +0100

    ok

--- spa_server.py
+++ spa_server.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""
-Prosty serwer HTTP dla Single Page Application
-Obsługuje routing SPA - wszystkie żądania przekierowuje do index.html
-"""
+
 import http.server
 import socketserver
 import os
@@ -16,13 +13,10 @@ class SPARequestHandler(http.server.SimpleHTTPRequestHandler):
         parsed_path = urlparse(self.path)
         path = parsed_path.path
         
-        # Jeśli żądanie dotyczy pliku, który fizycznie istnieje (np. .js, .css, obrazy)
-        # obsłuż je standardowo
+
         if os.path.exists('.' + path) and os.path.isfile('.' + path):
             return http.server.SimpleHTTPRequestHandler.do_GET(self)
-        
-        # W przeciwnym razie (np. /movie/1, /login, /profile)
-        # zwróć index.html, aby SPA mogło obsłużyć routing
+
         if not path.startswith('/js/') and not path.startswith('/css/') and not path.startswith('/images/'):
             self.path = '/index.html'
         
